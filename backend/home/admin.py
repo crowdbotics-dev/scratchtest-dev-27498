b870340d0d9e3118847de4ffdcd6d41822f23fba
Crowdbotics <engineering@crowdbotics.com>
2022-08-08 10:06:26 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Rasgjkf
+
+admin.site.register(Rasgjkf)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Rasgjkf(models.Model):
+    "Generated Model"
+    yuirtr = models.TextField()
